736c51c4b04400d28b12a65a308c94ed6777d3f8
karen <js785188@gmail.com>
2021-06-08 16:31:42 +0900

    サーバー立てた

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    text := "."
+    fmt.Fprint(w, "Hello World", text)
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
